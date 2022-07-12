961bd1894c3d8b06879195801849247e061de175
Jhonatan Maciel <82352924+emmnesty@users.noreply.github.com>
2022-07-12 11:08:22 -0300

    Listagem e Inserção no banco via aplicação

---
+++ src/TestaListagem.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaListagem {
+
+	public static void main(String[] args) throws SQLException {
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexao();
+
+		Statement stm = connection.createStatement();
+		// Essa lista vai me retornar um booleano, no caso da consulta.
+		// Caso fosse del ou upd retornaria um boolean false.
+		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+
+		ResultSet rst = stm.getResultSet();
+
+		// Esse método Next vai percorrer um a um na lista que retornou, quando chegar
+		// no ultimo vai sair do laço
+		while (rst.next()) {
+			Integer id = rst.getInt("id");
+			System.out.println(id);
+			String nome = rst.getString("nome");
+			System.out.println(nome);
+			String descricao = rst.getString("descricao");
+			System.out.println(descricao);
+		}
+
+		connection.close();
+
+	}
+
+}
