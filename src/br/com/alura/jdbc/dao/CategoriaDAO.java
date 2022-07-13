e21bdc12379db64dc286ed52810c19b0a810d77f
Jhonatan Maciel <82352924+emmnesty@users.noreply.github.com>
2022-07-13 09:38:15 -0300

    Adicionando tb categoria e associando chave estrangeira

---
+++ src/br/com/alura/jdbc/dao/CategoriaDAO.java
@@ -0,0 +1,38 @@
+package br.com.alura.jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.alura.jdbc.modelo.Categoria;
+
+public class CategoriaDAO {
+	
+	private Connection connection;
+
+	public CategoriaDAO(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public List<Categoria> listar() throws SQLException {
+		List<Categoria> categorias = new ArrayList<>();
+		
+		String sql = "SELECT ID, NOME FROM CATEGORIA";
+		
+		try (PreparedStatement psmt = connection.prepareStatement(sql)) {
+			psmt.execute();
+			
+			try(ResultSet rst = psmt.getResultSet()) {
+				while(rst.next()) {
+					Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
+					categorias.add(categoria);
+				}
+			}
+		}
+		return categorias;
+	}
+
+}
